b55ecca829033363881f496c78facee8a8fcfef5
touheedullahk <touheedullahkalhoro1@gmail.com>
2026-05-30 20:59:21 +0300

    Added SHA256 password hashing service

--- Password Brute ForceApp/Form1.cs
+++ Password Brute ForceApp/Form1.cs
@@ -1,3 +1,5 @@
+using PasswordBruteForceApp.Services;
+
 namespace Password_Brute_ForceApp
 {
     public partial class Form1 : Form
@@ -5,6 +7,15 @@ namespace Password_Brute_ForceApp
         public Form1()
         {
             InitializeComponent();
+            PasswordHasher hasher = new PasswordHasher();
+            string testHash = hasher.HashPassword("test");
+
+            MessageBox.Show(testHash, "SHA256 Test Hash");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ Password Brute ForceApp/Services/PasswordHasher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordBruteForceApp.Services
+{
+    public class PasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            if (password == null)
+            {
+                throw new ArgumentNullException(nameof(password), "Password cannot be null.");
+            }
+
+            string saltedPassword = AppSettings.StaticSalt + password;
+
+            using SHA256 sha256 = SHA256.Create();
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(saltedPassword);
+            byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+            return ConvertToHexString(hashBytes);
+        }
+
+        private string ConvertToHexString(byte[] bytes)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (byte b in bytes)
+            {
+                builder.Append(b.ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
