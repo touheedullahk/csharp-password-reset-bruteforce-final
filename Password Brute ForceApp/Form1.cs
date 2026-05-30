2f1fa804f427da029d1391cc8da22078d2acc1a4
touheedullahk <touheedullahkalhoro1@gmail.com>
2026-05-31 01:54:26 +0300

    Add brute-force combination generator

--- Password Brute ForceApp/Form1.cs
+++ Password Brute ForceApp/Form1.cs
@@ -7,24 +7,6 @@ namespace Password_Brute_ForceApp
         public Form1()
         {
             InitializeComponent();
-
-            PasswordHasher hasher = new PasswordHasher();
-            PasswordValidator validator = new PasswordValidator();
-
-            string originalPassword = "abc1";
-            string hash = hasher.HashPassword(originalPassword);
-
-            bool correctTest = validator.IsPasswordMatch("abc1", hash);
-            bool wrongTest = validator.IsPasswordMatch("wrong", hash);
-
-            MessageBox.Show(
-                $"Original Password: {originalPassword}\n" +
-                $"Hash: {hash}\n\n" +
-                $"Correct password test: {correctTest}\n" +
-                $"Wrong password test: {wrongTest}",
-                "Password Validator Test"
-                );
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
---
+++ Password Brute ForceApp/Services/BruteForceGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PasswordBruteForceApp.Services
+{
+    public class BruteForceGenerator
+    {
+        public IEnumerable<string> GenerateAllCombinations()
+        {
+            for (int length = AppSettings.BruteForceMinLength; length <= AppSettings.BruteForceMaxLength; length++)
+            {
+                foreach (string combination in GenerateCombinationsByLength(length))
+                {
+                    yield return combination;
+                }
+            }
+        }
+
+        public IEnumerable<string> GenerateCombinationsByLength(int length)
+        {
+            if (length < 1)
+            {
+                throw new ArgumentException("Length must be at least 1.", nameof(length));
+            }
+
+            char[] currentCombination = new char[length];
+
+            foreach (string combination in GenerateRecursive(currentCombination, 0, length))
+            {
+                yield return combination;
+            }
+        }
+
+        private IEnumerable<string> GenerateRecursive(char[] currentCombination, int position, int length)
+        {
+            if (position == length)
+            {
+                yield return new string(currentCombination);
+                yield break;
+            }
+
+            foreach (char character in AppSettings.CharacterSet)
+            {
+                currentCombination[position] = character;
+
+                foreach (string combination in GenerateRecursive(currentCombination, position + 1, length))
+                {
+                    yield return combination;
+                }
+            }
+        }
+    }
+}
